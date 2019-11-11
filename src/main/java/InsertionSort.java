d191c1c5c842e5876ca879654a580b452318343f
Nick Edwards <nicholas6598@gmail.com>
2019-11-11 16:28:02 -0500

    Initial commit

---
+++ src/main/java/InsertionSort.java
@@ -0,0 +1,119 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
+class Node {
+    int val;
+    Node next;
+
+    Node (int x, Node n) {
+        val = x;
+        next = n;
+    }
+
+    Node (int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class InsertionSort {
+    public static void main(String [] args) {
+        InsertionSort ui = new InsertionSort();
+        File f = new File(args[0]);
+        try {
+            Node head = getNumbers(f);
+            ui.sort(head,0);
+            print(head);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private static Node getNumbers(File f)throws IOException{
+        Scanner fScanner = new Scanner(f);
+        Node head = new Node(fScanner.nextInt());
+        Node current = head;
+        while(fScanner.hasNextInt()){
+           current.next = new Node(fScanner.nextInt());
+           current = current.next;
+        }
+        return head;
+    }
+
+    private void sort(Node head,int sorted){
+        Node current = head;
+        Node key = head;
+        for(int i =0; i<=sorted; i++)
+            key=key.next;
+
+
+        while(key!=null){
+            remove(head,key);
+            for(int i =0; i<=sorted; ++i) {
+                if (current == head && key.val > current.val) {
+                    //if largest put at start
+                    key.next = current;
+                    sort(key, ++sorted);
+                } else if (current.val < key.val && current.next.val > key.val) {
+                    //insert between current and current.next
+                    insert(current, key);
+                    sort(head, ++sorted);
+                } else if (current.next == null) {
+                    //if smallest put at end
+                    current.next = key;
+                    sort(head, ++sorted);
+                } else
+                    current = current.next;
+            }
+
+        }
+    }
+
+    private void remove(Node head, Node rm){
+        Node current = head;
+        while(true){
+            if(current.next == rm){
+                current.next = current.next.next;
+                break;
+            }
+            else
+                current = current.next;
+        }
+    }
+
+    private void insert(Node lead, Node inserted){
+        Node tmp = lead;
+        lead.next = inserted;
+        inserted.next = tmp.next;
+    }
+
+    private int getLength(Node head){
+        if(head == null)
+            return 0;
+        else{
+            Node current = head;
+            int count = 1;
+            while(current!=null) {
+                if (current.next == null) return count;
+                else {
+                    current = current.next;
+                    count++;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static void print(Node head){
+        Node current = head;
+        while(current.next!=null){
+            System.out.print(current.val+" ");
+            current = current.next;
+        }
+        System.out.print(current.val);
+    }
+}
---
+++ src/main/java/InsertionSortBeta.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class InsertionSortBeta {
+    public static void main(String [] args){
+        InsertionSortBeta ui = new InsertionSortBeta();
+        File f = new File(args[0]);
+        try{
+            LinkedList <Integer> list = getNumbers(f);
+            ui.sort(list);
+            print(list);
+        }catch(IOException e){e.printStackTrace();System.exit(1);}
+    }
+
+    private static LinkedList<Integer> getNumbers(File f)throws IOException{
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        Scanner fwriter = new Scanner(f);
+        while(fwriter.hasNext()){
+            String line = fwriter.nextLine();
+            StringBuilder k = new StringBuilder();
+            for(int c = 0; c<line.length(); c++){
+                if(line.charAt(c)==' ') {
+                    l.add(Integer.parseInt(k.toString()));
+                    k.delete(0,k.length());
+                }
+                else
+                    k.append(line.charAt(c));
+            }
+            l.add(Integer.parseInt(k.toString()));
+        }
+        fwriter.close();
+        return l;
+    }
+
+    private void sort(LinkedList<Integer> list){
+        int n = list.size();
+        for (int i = 1; i < n; ++i) {
+            int key = list.get(i);
+            int j = i - 1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && list.get(j) > key) {
+                list.set(j + 1,list.get(j));
+                j = j - 1;
+            }
+            list.set(j + 1,key);
+        }
+    }
+
+    private static void print(LinkedList<Integer> list){
+        StringBuilder k = new StringBuilder();
+        for(int i: list){
+            k.append(i);
+            k.append(" ");
+        }
+        k.delete(k.lastIndexOf(" "),k.lastIndexOf(" ")+1);
+        System.out.print(k.toString());
+    }
+}
